2507f63a2f08b7fafe85d7ee72c8327505bbde63
Gabriel Guesser <gabrieldguesser@hotmail.com>
2021-04-28 22:00:40 -0300

    using lombok, removing all generic methods from the entities

--- src/main/java/com/guesser/myfinances/model/entity/Release.java
+++ src/main/java/com/guesser/myfinances/model/entity/Release.java
@@ -1,10 +1,11 @@
 package com.guesser.myfinances.model.entity;
 
+import lombok.Data;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "releases")
 public class Release {
@@ -29,104 +30,4 @@ public class Release {
 
     @Enumerated(value = EnumType.STRING)
     private ReleaseStatus status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Integer getMonth() {
-        return month;
-    }
-
-    public void setMonth(Integer month) {
-        this.month = month;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
-
-    public LocalDate getDateRegister() {
-        return dateRegister;
-    }
-
-    public void setDateRegister(LocalDate dateRegister) {
-        this.dateRegister = dateRegister;
-    }
-
-    public ReleaseType getType() {
-        return type;
-    }
-
-    public void setType(ReleaseType type) {
-        this.type = type;
-    }
-
-    public ReleaseStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ReleaseStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Release release = (Release) o;
-        return Objects.equals(id, release.id) && Objects.equals(description, release.description) && Objects.equals(year, release.year) && Objects.equals(month, release.month) && Objects.equals(user, release.user) && Objects.equals(value, release.value) && Objects.equals(dateRegister, release.dateRegister) && type == release.type && status == release.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, description, year, month, user, value, dateRegister, type, status);
-    }
-
-    @Override
-    public String toString() {
-        return "Release{" +
-                "id=" + id +
-                ", description='" + description + '\'' +
-                ", year=" + year +
-                ", month=" + month +
-                ", user=" + user +
-                ", value=" + value +
-                ", dateRegister=" + dateRegister +
-                ", type=" + type +
-                ", status=" + status +
-                '}';
-    }
 }
--- src/main/java/com/guesser/myfinances/model/entity/User.java
+++ src/main/java/com/guesser/myfinances/model/entity/User.java
@@ -1,8 +1,9 @@
 package com.guesser.myfinances.model.entity;
 
+import lombok.Data;
 import javax.persistence.*;
-import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -14,49 +15,4 @@ public class User {
     private String name;
     private String email;
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, password);
-    }
 }
